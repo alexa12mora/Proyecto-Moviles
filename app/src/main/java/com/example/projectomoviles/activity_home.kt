08678d75b855b20e8bcb25f739164ce87909b930
JRojas89 <J89Rojas@gmail.com>
2023-06-17 22:09:33 -0600

    Home done

--- app/src/main/java/com/example/projectomoviles/activity_home.kt
+++ app/src/main/java/com/example/projectomoviles/activity_home.kt
@@ -1,9 +1,10 @@
 package com.example.projectomoviles
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.projectomoviles.calendario.Event
 import com.example.projectomoviles.calendario.calendario
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -29,6 +30,74 @@ class activity_home : Base_Activity(){
                 else -> false
             }
         }
+
+        val boxUser = findViewById<LinearLayout>(R.id.boxUser)
+        val boxCalendar = findViewById<LinearLayout>(R.id.boxCalendar)
+        val boxNotification = findViewById<LinearLayout>(R.id.boxNotification)
+        val boxMain = findViewById<LinearLayout>(R.id.boxMain)
+        val tvUser = findViewById<TextView>(R.id.tvUser)
+        val tvCalendar = findViewById<TextView>(R.id.tvCalendar)
+        val tvNotifications = findViewById<TextView>(R.id.tvNotifications)
+        val tvLogout = findViewById<TextView>(R.id.tvLogout)
+
+        boxUser.setOnClickListener {
+            val intent = Intent(this, activity_user_d::class.java)
+            Event.eventsList.clear()
+            startActivity(intent)
+            finish();
+        }
+
+        tvUser.setOnClickListener {
+            val intent = Intent(this, activity_user_d::class.java)
+            Event.eventsList.clear()
+            startActivity(intent)
+            finish();
+        }
+
+        boxCalendar.setOnClickListener {
+            val intent = Intent(this, calendario::class.java)
+            Event.eventsList.clear()
+            startActivity(intent)
+            finish();
+        }
+
+        tvCalendar.setOnClickListener {
+            val intent = Intent(this, calendario::class.java)
+            Event.eventsList.clear()
+            startActivity(intent)
+            finish();
+        }
+
+        boxNotification.setOnClickListener {
+            // Cambiar calendario::class.java -> por el de notificación cuando esté hecho.
+            val intent = Intent(this, calendario::class.java)
+            Event.eventsList.clear()
+            startActivity(intent)
+            finish();
+        }
+
+        tvNotifications.setOnClickListener {
+            // Cambiar calendario::class.java -> por el de notificación cuando esté hecho.
+            val intent = Intent(this, calendario::class.java)
+            Event.eventsList.clear()
+            startActivity(intent)
+            finish();
+        }
+
+        boxMain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            Event.eventsList.clear()
+            startActivity(intent)
+            finish();
+        }
+
+        tvLogout.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            Event.eventsList.clear()
+            startActivity(intent)
+            finish();
+        }
+
     }
     private fun redirectToLoginActivity() {
         val intent = Intent(this, MainActivity::class.java)
